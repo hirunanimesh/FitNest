d37b84113e3a976be6dca4a8bb8ce58aae72a952
Vidura2002 <viduramalshan185@gmail.com>
2025-08-01 02:05:42 +0530

    add:feat:gymService
    add GymService features related to gyms and gym plans

---
+++ backend/GymService/controllers/plans.controller.js
@@ -0,0 +1,74 @@
+import { addgymplan, deletegymplan, getallgymplans, getgymplanbygymid, updategymplan } from "../services/plans.service.js";
+
+
+export const addGymPlan = async (req, res) => {
+    try {
+        const gymPlan = await addgymplan(req.body);
+        if (gymPlan) {
+            res.status(200).json({ message: "Gym plan created successfully", gymPlan });
+        }
+    } catch (error) {
+        console.error("Error creating gym plan:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export const getAllGymPlans = async (req, res) => {
+    try {
+        const gymPlans = await getallgymplans()
+        if (gymPlans) {
+            res.status(200).json({ message: "Gym plans retrieved successfully", gymPlans });
+        } else {
+            res.status(404).json({ message: "No gym plans found" });
+        }
+    } catch (error) {
+        console.error("Error retrieving gym plans:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export const getGymPlanByGymId = async (req, res) => {
+    const { gymId } = req.params;
+    try {
+        const gymPlan = await getgymplanbygymid(gymId)
+        if (gymPlan) {
+            res.status(200).json({ message: "Gym plan retrieved successfully", gymPlan });
+        } else {
+            res.status(404).json({ message: "Gym plan not found" });
+        }
+    } catch (error) {
+        console.error("Error retrieving gym plan:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+export const updateGymPlan = async (req, res) => {
+    const { gymPlanId } = req.params;
+    try {
+        const updatedGymPlan = await updategymplan(gymPlanId, req.body);
+        if (updatedGymPlan) {
+            res.status(200).json({ message: "Gym plan updated successfully", updatedGymPlan });
+        } else {
+            res.status(404).json({ message: "Gym plan not found" });
+        }
+    } catch (error) {
+        console.error("Error updating gym plan:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+export const deleteGymPlan = async (req, res) => {
+    const { gymPlanId } = req.params;
+    try {
+        const deleteplan = await deletegymplan(gymPlanId);
+        if(deleteplan){
+            res.status(200).json({ message: "Gym plan deleted successfully" });
+        }else{
+            res.status(404).json({ message: "Gym plan not found" });
+        }
+
+    } catch (error) {
+        console.error("Error deleting gym plan:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
---
+++ backend/GymService/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import { addGym,getAllGyms, getGymById, updateGymDetails } from './controllers/gym.controller.js'
+import { addGymPlan, deleteGymPlan, getAllGymPlans, getGymPlanByGymId, updateGymPlan } from './controllers/plans.controller.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+app.post('/addGym',addGym)
+app.get('/getallgyms',getAllGyms)
+app.get('/getgymbyid/:gymId',getGymById)
+app.put('/updategymdetails/:gymId',updateGymDetails)
+
+app.post('/addgymplan', addGymPlan)
+app.get('/getallgymplans',getAllGymPlans)
+app.get('/getgymplanbygymid/:gymId',getGymPlanByGymId)
+app.put('/updategymplan/:gymPlanId', updateGymPlan)
+app.delete('/deletegymplan/:gymPlanId',deleteGymPlan)
+
+app.listen(process.env.PORT || 3002,()=>{
+    console.log(`Gym Service is running on port ${process.env.PORT || 3002}`)
+})
