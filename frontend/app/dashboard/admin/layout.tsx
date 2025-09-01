4e142da1fd40b4bd33fa4e5f9af9fac706f14101
hirunanimesh <158060469+hirunanimesh@users.noreply.github.com>
2025-09-01 22:24:28 +0530

    protect admin

--- frontend/app/dashboard/admin/layout.tsx
+++ frontend/app/dashboard/admin/layout.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { AdminNavbar } from "@/components/admin-navbar"
+import { ProtectedRoute } from "@/components/ProtectedRoute"
 
 const Adminlayout = ({children}:{children:React.ReactNode}) => {
   return (
     <div>
+      <ProtectedRoute allowedRoles={['admin']}>
       <AdminNavbar/>
       <div>
         {children}
       </div>
+      </ProtectedRoute>
     </div>
   )
 }
