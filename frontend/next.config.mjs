2a10139167f37b8d4edd3c1ab6926cfc31ca37ce
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-09-02 03:30:46 +0530

    Remove next-pwa dependency from package.json

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,3 @@
-import withPWA from 'next-pwa'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -11,53 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Suppress useLayoutEffect warning from Next.js dev overlay
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.module.rules.push({
-        test: /\.tsx?$/,
-        loader: 'string-replace-loader',
-        options: {
-          search: 'useLayoutEffect',
-          replace: 'useEffect',
-          flags: 'g',
-        },
-        include: [
-          /node_modules\/next\/dist\/client\/components\/react-dev-overlay/
-        ]
-      })
-    }
-    return config
-  },
-  // Note: For port configuration, it's better to use package.json scripts
-  // This config is for other development settings
 }
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: false, // Always enabled for PWA to work properly
-  buildExcludes: [
-    /middleware-manifest\.json$/,
-    /app-build-manifest\.json$/,
-    /_buildManifest\.js$/,
-    /_ssgManifest\.js$/
-  ],
-  publicExcludes: ['!robots.txt', '!sitemap.xml'],
-  // Add additional PWA configurations
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 50,
-        },
-      },
-    },
-  ],
-})
-
-export default pwaConfig(nextConfig)
+export default nextConfig
