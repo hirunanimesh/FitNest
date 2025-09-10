23c484a0059442e7e43f4ff8990972f99f5690e3
Vidura2002 <viduramalshan185@gmail.com>
2025-09-11 00:47:14 +0530

    Update Kafka.js

--- backend/TrainerService/config/Kafka.js
+++ backend/TrainerService/config/Kafka.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 
 export const kafka = new Kafka({
     clientId: 'my-node-client',
-    brokers: ["fitnest-kafka-developmentproject38-c6e7.d.aivencloud.com:25322"], // use your actual host and port
+    brokers: [process.env.KAFKR], // use your actual host and port
     ssl: {
       rejectUnauthorized: true,
       ca: [fs.readFileSync('./ca.pem', 'utf-8')], // path to your Aiven CA certificate
@@ -12,6 +12,6 @@ export const kafka = new Kafka({
     sasl: {
       mechanism: 'plain',
       username: 'avnadmin', // replace with your Aiven username
-      password: 'AVNS_RKqPRvi8HVb_I6KGplz', // replace with your Aiven password
+      password: process.env.KAFKA_PASSWORD, // replace with your Aiven password
     },
   });
