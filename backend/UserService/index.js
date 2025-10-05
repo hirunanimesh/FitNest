5d8135e06e899b01469e13edbc7f7102dcc2da33
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-10-05 14:04:47 +0530

    save

--- backend/UserService/index.js
+++ backend/UserService/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
-import {addfeedback,getlatestweightbyid,getweightbyid,addweight,getuserbyid, updateuserdetails} from './controllers/user.controller.js'
+import {addfeedback,getlatestweightbyid,getweightbyid,addweight,getuserbyid, updateuserdetails, getMySessions, addreport} from './controllers/user.controller.js'
 import {
 	getGoogleOauthUrl,
 	googleCallback,
@@ -25,7 +25,8 @@ app.post('/addweight',addweight)
 app.get('/getweightbyid/:userId',getweightbyid)
 app.get('/getlatestweightbyid/:userId',getlatestweightbyid)
 app.post('/addfeedback',addfeedback)
-
+app.get('/mysessions/:customerId',getMySessions)
+app.post('/addreport',addreport)  
 // Calendar / Google OAuth endpoints
 app.get('/google/oauth-url/:userId', getGoogleOauthUrl)
 app.get('/google/callback', googleCallback)
